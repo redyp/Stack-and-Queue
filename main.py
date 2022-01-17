7f129fdd2d5f79b2bd6a38b86f0ec5a8d9e1c9d2
redyp <redy.p@students.amikom.ac.id>
2022-01-17 18:27:47 +0700

    add class Queue to main.py

--- main.py
+++ main.py
@@ -46,3 +46,28 @@ class Stack:
         temp._next = None
         self.lenght -= 1
         return temp
+
+@dataclass
+class Queue:
+    value: int = field(repr=False)
+    head: Node = field(init=False)
+    tail: Node = field(init=False)
+    lenght: int = field(init=False)
+    
+    def __post_init__(self):
+        new_node = Node(self.value)
+        self.head = new_node
+        self.tail = new_node
+        self.lenght = 1
+        
+    def __str__(self) -> str:
+        if self.head is None:
+            return "Queue Kosong"
+        temp = self.head
+        temp_str = ""
+        while temp:
+            temp_str += f"{temp.value } -> "
+            temp = temp._next
+        temp_str += "None"
+        return temp_str
+    
